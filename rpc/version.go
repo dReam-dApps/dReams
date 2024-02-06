ee8ba4803fa1ac19d083e548354812cb33f1c12c
Six <84689659+SixofClubsss@users.noreply.github.com>
2024-02-06 09:44:52 +0000

    minter parse all contracts

--- rpc/version.go
+++ rpc/version.go
@@ -2,7 +2,7 @@ package rpc
 
 import "github.com/blang/semver/v4"
 
-var dreamsV = semver.MustParse("0.11.1-dev.2")
+var dreamsV = semver.MustParse("0.11.1-dev.3")
 
 // Get current package version
 func Version() semver.Version {
